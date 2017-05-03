239db7eeffc18925130981e54ee6d194298d9787
hyc <875224980@qq.com>
2017-05-03 15:40:11 +0800

    Initial commit

---
+++ app/src/main/java/com/mrw/headzoomscrollview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mrw.headzoomscrollview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView iv = (ImageView) findViewById(R.id.iv);
+        Glide.with(this).load("http://img.ivsky.com/img/tupian/pre/201612/29/haohan_xingyun_yuzhou.jpg").into(iv);
+    }
+
+}
